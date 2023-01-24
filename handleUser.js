d0d31b2892cbf6e4ba91ac339e23c65d61cc62b2
Max Mandia <mmandia511@gmail.com>
2023-01-24 16:05:19 -0500

    feat: added url redirect for signup

--- handleUser.js
+++ handleUser.js
@@ -1,4 +1,5 @@
 const admin = require("./firebase");
+const client = twilio(process.env.SID, process.env.TWILLIOKEY);
 
 module.exports = {
   checkIfUserExists: async function (phone) {
@@ -10,20 +11,25 @@ module.exports = {
     if (response.exists) {
       // handle logic yea
     } else {
-      admin.admin
-        .firestore()
-        .collection("users")
-        .doc(phone)
-        .create({
-          phoneNumber: phone,
-          createdAt: new Date(),
-        })
-        .then(() => {
-          console.log("user created");
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+      client.messages({
+        to: phone,
+        from: "+18126055420",
+        body: `Welcome to MeAndGPT! Visit www.meandgpt.com/signup/${phone} to get started`,
+      });
+      // admin.admin
+      //   .firestore()
+      //   .collection("users")
+      //   .doc(phone)
+      //   .create({
+      //     phoneNumber: phone,
+      //     createdAt: new Date(),
+      //   })
+      //   .then(() => {
+      //     console.log("user created");
+      //   })
+      //   .catch((e) => {
+      //     console.log(e);
+      //   });
     }
   },
 };
