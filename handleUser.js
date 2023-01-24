eb0760b9ad785ab5d9aa2141b091eb1d3d6eeba7
Max Mandia <mmandia511@gmail.com>
2023-01-24 12:06:56 -0500

    new fix

--- handleUser.js
+++ handleUser.js
@@ -2,11 +2,7 @@ const admin = require("firebase-admin");
 
 module.exports = {
   checkIfUserExists: async function (user) {
-    let response = await admin()
-      .firestore()
-      .collection("users")
-      .doc(user)
-      .get();
+    let response = await admin.firestore().collection("users").doc(user).get();
     console.log(response.docs.length);
   },
 };
