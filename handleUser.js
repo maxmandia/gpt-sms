44bf29c256afbd842d4713d20f87dbc853647550
Max Mandia <mmandia511@gmail.com>
2023-01-24 12:52:51 -0500

    hotfix

--- handleUser.js
+++ handleUser.js
@@ -10,7 +10,7 @@ module.exports = {
     if (response.exists) {
       // handle logic
     } else {
-      admin
+      admin.admin
         .firestore()
         .collection("users")
         .doc(phone)
