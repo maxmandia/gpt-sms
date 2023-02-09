62af6684d17de51fdb71cfd8e8d9d78b7d4dba61
Max Mandia <mmandia511@gmail.com>
2023-02-09 16:54:30 -0500

    hotfix

--- index.js
+++ index.js
@@ -27,33 +27,33 @@ app.use(function (req, res, next) {
 app.post("/message", (req, res) => {
   let smsText = req.body.Body;
   let smsPhone = req.body.From;
-  handleUser.checkIfUserExists(smsPhone);
+  // handleUser.checkIfUserExists(smsPhone);
 
-  // openai
-  //   .createCompletion({
-  //     model: "text-davinci-003",
-  //     prompt: smsText,
-  //     max_tokens: 1000,
-  //     temperature: 0,
-  //   })
-  //   .then((resp) => {
-  //     let answer = resp.data.choices[0].text;
-  //     console.log(answer);
+  openai
+    .createCompletion({
+      model: "text-davinci-003",
+      prompt: smsText,
+      max_tokens: 1000,
+      temperature: 0,
+    })
+    .then((resp) => {
+      let answer = resp.data.choices[0].text;
+      console.log(answer);
 
-  //     client.messages
-  //       .create({
-  //         to: smsPhone,
-  //         from: "+18126055420",
-  //         body: answer,
-  //       })
-  //       .then(() => {
-  //         console.log("message sent");
-  //       })
-  //       .catch((e) => console.log(e));
-  //   })
-  //   .catch((e) => {
-  //     console.log(e.response.data);
-  //   });
+      client.messages
+        .create({
+          to: smsPhone,
+          from: "+18126055420",
+          body: answer,
+        })
+        .then(() => {
+          console.log("message sent");
+        })
+        .catch((e) => console.log(e));
+    })
+    .catch((e) => {
+      console.log(e.response.data);
+    });
 });
 
 app.post("/create-payment-intent", async (req, res) => {
